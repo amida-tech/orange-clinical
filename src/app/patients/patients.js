6629af6849464c747e1da8b9088ee19239fecc1f
Harry Rickards <rickards@mit.edu>
2015-10-08 18:23:03 -0400

    Search patients by last name

--- src/app/patients/patients.js
+++ src/app/patients/patients.js
@@ -44,20 +44,45 @@ angular.module( 'orangeClinical.patients', [
   // get all patients
   $scope.patients = [];
   $scope.patientsCount = 0;
-  $scope.$watch("currentPage", function (newPage) {
-    Patients.query({
-      limit: $scope.patientsPerPage,
-      // 0-indexing
-      offset: (newPage - 1) * $scope.patientsPerPage
-    }, function (res) {
+  $scope.query = {};
+  $scope.getPatients = function () {
+    Patients.query($scope.query, function (res) {
       $scope.patients = res.patients;
       $scope.patientsCount = res.count;
     });
-  });
+  };
 
   // pagination
-  $scope.currentPage = 1;
   $scope.patientsPerPage = 5;
+  $scope.currentPage = 1;
+  $scope.$watch('currentPage', function (newPage) {
+    $scope.query.limit = $scope.patientsPerPage;
+    // 0-indexing
+    $scope.query.offset = (newPage - 1) * $scope.patientsPerPage;
+    $scope.getPatients();
+  });
+
+  // searching
+  $scope.lastName = "";
+  $scope.search = function () {
+    var oldQuery = angular.copy($scope.query);
+
+    if ($scope.lastName.length >= 3) {
+      $scope.query.last_name = $scope.lastName;
+    } else {
+      delete $scope.query.last_name;
+    }
+
+    // don't query patients if the two queries are identical
+    // prevents making duplicate requests when typing <3 chars
+    if (!angular.equals($scope.query, oldQuery)) {
+      // if we are making a query that's different in name,
+      // go back to the first page
+      $scope.currentPage = 1;
+
+      $scope.getPatients();
+    }
+  };
 })
 
 // single patient detail view controller
